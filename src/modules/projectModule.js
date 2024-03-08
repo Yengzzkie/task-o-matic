a4cf9f6bc341bd9b1a1256ed4e5ab3977a93d776
Yengzzkie <gatchalian.manuel@gmail.com>
2024-03-08 11:38:14 -0500

    feature/added logic for rendering project objects

--- src/modules/projectModule.js
+++ src/modules/projectModule.js
@@ -1,21 +1,23 @@
 
-let projects = [{title: "Work", description: "Go to work"}, {title: "Read", description: "Read some books"}];
+import renderProjects from "./renderProjects";
+
+let projects = [{title: "Work", description: "Go to work", todo: []}, {title: "Read", description: "Read some books", todo: []}];
 
 export default function projectModule() {
     class Project { //main constructor for Project
         constructor(title, description) {
             this.title = title,
-            this.description = description
+            this.description = description,
+            this.todo = []
         }
     }
 
-    projects.forEach(project => { //loop through the array and console log the objects
-        console.log(project);
-    });
+    renderProjects(projects);
 
-    const newProject = new Project("Run", "Jog for 5 mins");
-        projects.push(newProject);
-        console.log(projects);
+    // const newProject = new Project("Run", "Jog for 5 mins");
+    //     projects.push(newProject);
+    //     console.log(projects);
+    //separate this to another module :)
 
     return Project;
 };
