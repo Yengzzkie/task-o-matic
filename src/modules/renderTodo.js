a325b3da98f2d573c4a8fb4a5440568c6f2f15a9
Yengzzkie <gatchalian.manuel@gmail.com>
2024-03-20 18:21:28 -0400

    feature/added warning modal when textarea is empty

--- src/modules/renderTodo.js
+++ src/modules/renderTodo.js
@@ -1,131 +1,140 @@
-import '../css/renderTodo.css';
-import renderProjects from './renderProjects.js';
+import "../css/renderTodo.css";
+import renderProjects from "./renderProjects.js";
+import warningModal from "./warningModal.js";
 
 export default function renderTodo(index) {
-    const projectsJSON = localStorage.getItem('projects');
-    const projects = JSON.parse(projectsJSON);
-    const project = projects[index]; //get the index of the projects
+  const projectsJSON = localStorage.getItem("projects");
+  const projects = JSON.parse(projectsJSON);
+  const project = projects[index]; //get the index of the projects
 
-    //this block below are all the variables
-    const app = document.getElementById('app');
-    const inputModal = document.createElement('dialog')
-    const todoContainer = document.createElement('div');
-    const todoInput = document.createElement('textarea');
-    const addTodoBtn = document.createElement('button');
-    const closeModalBtn = document.createElement('button');
-    const openModalBtn = document.createElement('button');
-    const backBtn = document.createElement('button');
-    const ul = document.createElement('ul');
-    const categoryTitle = document.createElement('h1');
-    const tasks = project.todo;
+  //this block below are all the variables
+  const app = document.getElementById("app");
+  const inputModal = document.createElement("dialog");
+  const todoContainer = document.createElement("div");
+  const todoInput = document.createElement("textarea");
+  const addTodoBtn = document.createElement("button");
+  const closeModalBtn = document.createElement("button");
+  const openModalBtn = document.createElement("button");
+  const backBtn = document.createElement("button");
+  const ul = document.createElement("ul");
+  const categoryTitle = document.createElement("h1");
+  const tasks = project.todo;
 
-    //this block below are the variable text assignment, separating them from the variables to avoid confusion
-    inputModal.className = 'input-modal';
-    addTodoBtn.textContent = 'Add Task';
-    closeModalBtn.textContent = 'Cancel';
-    todoContainer.className = 'todo-container';
-    openModalBtn.innerHTML = '<span class="material-symbols-outlined">add_notes</span>';
-    openModalBtn.setAttribute('id', 'add-task');
-    backBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-    backBtn.className = 'back-btn'
-    categoryTitle.innerHTML = `${project.title} Notes <span class="material-symbols-outlined">menu_book</span>`;
+  //this block below are the variable text assignment, separating them from the variables to avoid confusion
 
-    openModalBtn.addEventListener('click', () => {
-      inputModal.showModal();
-    })
+  inputModal.className = "input-modal";
+  addTodoBtn.textContent = "Add Task";
+  closeModalBtn.textContent = "Cancel";
+  todoContainer.className = "todo-container";
+  openModalBtn.innerHTML =
+    '<span class="material-symbols-outlined">add_notes</span>';
+  openModalBtn.setAttribute("id", "add-task");
+  backBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  backBtn.className = "back-btn";
+  categoryTitle.innerHTML = `${project.title} Notes <span class="material-symbols-outlined">menu_book</span>`;
 
-    closeModalBtn.addEventListener('click', () => {
-      inputModal.close();
-    })
+  openModalBtn.addEventListener("click", () => {
+    inputModal.showModal();
+  });
 
-    addTodoBtn.addEventListener('click', addTask);
+  closeModalBtn.addEventListener("click", () => {
+    inputModal.close();
+  });
 
-    //main function in adding new tasks
+  addTodoBtn.addEventListener("click", addTask);
+
+  //main function in adding new tasks
   function addTask() {
     const todoInputValue = todoInput.value;
-    const newDate = new Date();
-    const postedDate = `${newDate.getFullYear()}-${newDate.getMonth()}-${newDate.getDate()} / ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}` ;
-    tasks.push({todo: todoInputValue, date: postedDate}); 
-    project.todo = tasks; // Update the project's todo array with the updated tasks
-    localStorage.setItem('projects', JSON.stringify(projects)); //store the new added tasks to the local storage
-    renderTodo(index); //then re-render the array by calling renderTodo function which contains the loadProjectsFromLocalStorage() function
 
-    const lastTask = tasks[tasks.length - 1];
-    console.log("Todo:", lastTask.todo);
-    console.log("Date:", lastTask.date);
+    if (!todoInputValue) {
+      warningModal();
+    } else {
+      const newDate = new Date();
+      const postedDate = `${newDate.getFullYear()}-${newDate.getMonth()}-${newDate.getDate()} / ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}`;
+      tasks.push({ todo: todoInputValue, date: postedDate });
+      project.todo = tasks; // Update the project's todo array with the updated tasks
+      localStorage.setItem("projects", JSON.stringify(projects)); //store the new added tasks to the local storage
+      renderTodo(index); //then re-render the array by calling renderTodo function which contains the loadProjectsFromLocalStorage() function
+
+      const lastTask = tasks[tasks.length - 1];
+      console.log("Todo:", lastTask.todo);
+      console.log("Date:", lastTask.date);
+    }
   }
 
-    backBtn.addEventListener('click', () => {
-      renderProjects();
-    }); //re-renders the projects when the back button is clicked
-  
-    // Clear the existing content in the app element to prevent duplicating of contents on re-render
-    app.innerHTML = '';
-    
-    // Iterate through the todo array of the project and create list items for each todo
-    tasks.forEach((todoItem, index) => {
-        const li = document.createElement('li');
-        const todoContainer = document.createElement('div')
-        const todoProp = document.createElement('p');
-        const dateProp = document.createElement('p');
-        const deleteModal = document.createElement('dialog');
-        const buttonWrapper = document.createElement('div');
-        const openDeleteModalBtn = document.createElement('button');
-        const deleteBtn = document.createElement('button');
-        const cancelBtn = document.createElement('button');
-        const deleteModalConfirmation = document.createElement('span');
-        const taskToDelete = tasks[index]
+  backBtn.addEventListener("click", () => {
+    renderProjects();
+  }); //re-renders the projects when the back button is clicked
 
-        
-        openDeleteModalBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-        deleteBtn.innerHTML = 'Confirm';
-        cancelBtn.textContent = 'Cancel';
-        deleteModalConfirmation.textContent = 'Are you done with this task?';
-        todoProp.textContent = todoItem.todo;
-        dateProp.textContent = todoItem.date;
-        dateProp.className = 'date';
+  // Clear the existing content in the app element to prevent duplicating of contents on re-render
+  app.innerHTML = "";
 
-        todoContainer.append(todoProp, dateProp)
-        li.append(todoContainer);
+  // Iterate through the todo array of the project and create list items for each todo
+  tasks.forEach((todoItem, index) => {
+    const li = document.createElement("li");
+    const todoContainer = document.createElement("div");
+    const todoProp = document.createElement("p");
+    const dateProp = document.createElement("p");
+    const deleteModal = document.createElement("dialog");
+    const buttonWrapper = document.createElement("div");
+    const openDeleteModalBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
+    const cancelBtn = document.createElement("button");
+    const deleteModalConfirmation = document.createElement("span");
+    const taskToDelete = tasks[index];
 
-        function removeTask(taskIndex) {
-          tasks.splice(taskIndex, 1); // Remove the task from the tasks array
-          project.todo = tasks; // Update the project's todo array with the updated tasks
-          localStorage.setItem('projects', JSON.stringify(projects)); // Update localStorage with the updated projects
+    openDeleteModalBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    deleteBtn.innerHTML = "Confirm";
+    cancelBtn.textContent = "Cancel";
+    deleteModalConfirmation.textContent = "Are you done with this task?";
+    todoProp.textContent = todoItem.todo;
+    dateProp.textContent = todoItem.date;
+    dateProp.className = "date";
 
-          // Remove the task's corresponding list item from the DOM
-          ul.removeChild(ul.childNodes[taskIndex]);
-      
-          // If there are no more tasks, remove the dialog element and re-render only the current project
-          if (tasks.length === 0) {
-              inputModal.close(); // Close the dialog
-              inputModal.remove(); // Remove the dialog element from the DOM
-              renderProjects(); // Re-render only the current project
-          }
+    todoContainer.append(todoProp, dateProp);
+    li.append(todoContainer);
+
+    function removeTask(taskIndex) {
+      tasks.splice(taskIndex, 1); // Remove the task from the tasks array
+      project.todo = tasks; // Update the project's todo array with the updated tasks
+      localStorage.setItem("projects", JSON.stringify(projects)); // Update localStorage with the updated projects
+
+      // Remove the task's corresponding list item from the DOM
+      ul.removeChild(ul.childNodes[taskIndex]);
+
+      // If there are no more tasks, remove the dialog element and re-render only the current project
+      if (tasks.length === 0) {
+        inputModal.close(); // Close the dialog
+        inputModal.remove(); // Remove the dialog element from the DOM
+        renderProjects(); // Re-render only the current project
       }
+    }
 
-      openDeleteModalBtn.addEventListener('click', () => { //opens the delete confirmation modal
-        deleteModal.showModal();
-      })
-
-      cancelBtn.addEventListener('click', () => { //closes the delete confirmation modal
-        deleteModal.close();
-      })
-      
-      // Inside the forEach loop to create delete buttons for each task
-      deleteBtn.addEventListener('click', () => {
-          const taskIndex = tasks.indexOf(todoItem); // Find the index of the task to remove
-          removeTask(taskIndex); // Call the removeTask function with the index to remove
-      });
-      
-        buttonWrapper.append(deleteBtn, cancelBtn)
-        deleteModal.append(deleteModalConfirmation, buttonWrapper);
-        li.append(openDeleteModalBtn, deleteModal);
-        ul.append(li);
+    openDeleteModalBtn.addEventListener("click", () => {
+      //opens the delete confirmation modal
+      deleteModal.showModal();
     });
 
-    // Append the unordered list to the app element
-    inputModal.append(todoInput, addTodoBtn, closeModalBtn)
-    todoContainer.append(categoryTitle, ul, openModalBtn, backBtn)
-    app.append(todoContainer, inputModal);
+    cancelBtn.addEventListener("click", () => {
+      //closes the delete confirmation modal
+      deleteModal.close();
+    });
+
+    // Inside the forEach loop to create delete buttons for each task
+    deleteBtn.addEventListener("click", () => {
+      const taskIndex = tasks.indexOf(todoItem); // Find the index of the task to remove
+      removeTask(taskIndex); // Call the removeTask function with the index to remove
+    });
+
+    buttonWrapper.append(deleteBtn, cancelBtn);
+    deleteModal.append(deleteModalConfirmation, buttonWrapper);
+    li.append(openDeleteModalBtn, deleteModal);
+    ul.append(li);
+  });
+
+  // Append the unordered list to the app element
+  inputModal.append(todoInput, addTodoBtn, closeModalBtn);
+  todoContainer.append(categoryTitle, ul, openModalBtn, backBtn);
+  app.append(todoContainer, inputModal);
 }
--- src/modules/warningModal.js
+++ src/modules/warningModal.js
@@ -4,8 +4,10 @@ export default function warningModal() {
   const warningModal = document.createElement("dialog");
   const warningDialog = document.createElement("span");
   const warningCloseBtn = document.createElement("button");
+
+  warningModal.className = 'warning-modal';
   warningCloseBtn.textContent = "Close";
-  warningDialog.textContent = "Category and brief description is required";
+  warningDialog.textContent = "Please fill up the text field, do not procrastinate!";
 
   warningCloseBtn.addEventListener("click", () => {
     warningModal.close();
